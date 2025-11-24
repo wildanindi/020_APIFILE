a0a1a9bf56ce68980062bf6ea53f632e5f66bc2a
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-24 12:44:49 +0700

    menambah modul api route, dan menambah path untuk apinya

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
+const apiRoute = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', apiRoute);
 
 async function startServer() {
     await connectDatabase();
